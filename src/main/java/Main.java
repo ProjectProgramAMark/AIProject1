f9bd5f797b993406ac147a6434ab312018a43188
Mark Moussa <projectprogramamark@gmail.com>
2017-10-12 12:16:13 -0400

    added a bunch of stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
-package main.java;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        // gets input and reads in files
         getInput();
     }
 
@@ -17,9 +24,16 @@ public class Main {
         int solutionType;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter full path of locations.txt");
-        locationsFilePath = scanner.nextLine();
+        // debugging
+        locationsFilePath = "src/main/resources/locations.txt";
+        HashMap<String, ArrayList<String>> locationsFileContent = readFile(locationsFilePath, "locations");
+//        locationsFilePath = scanner.nextLine();
         System.out.println("Enter full path of connections.txt");
-        connectionsFilePath = scanner.nextLine();
+        // debugging
+        connectionsFilePath = "src/main/resources/connections.txt";
+//        connectionsFilePath = scanner.nextLine();
+        HashMap<String, ArrayList<String>> connectionsFileContent = readFile(connectionsFilePath, "connections");
+        DefaultDirectedWeightedGraph connectionsGraph = buildGraph(connectionsFileContent);
         System.out.println("Enter name of starting city");
         startCity = scanner.nextLine();
         System.out.println("Enter name of end city");
@@ -57,6 +71,96 @@ public class Main {
         }
     }
 
+    private static HashMap<String, ArrayList<String>> readFile(String filePath, String fileType) {
+        // Using Object because I need ArrayList<Integer> for one and ArrayList<String> for the other
+        String line = null;
+        HashMap<String, ArrayList<String>> fileContents = new HashMap<>();
+        if(fileType.equals("locations")) {
+            try {
+                FileReader fileReader = new FileReader(filePath);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                while(!(line = bufferedReader.readLine()).equals("END")) {
+                    ArrayList<String> splitArray = new ArrayList<>(Arrays.asList(line.split("\\s+")));
+                    String city = splitArray.get(0);
+                    splitArray.remove(0);
+                    // debugging
+//                    System.out.println(splitArray);
+
+                    /* Commenting this out because it helps to just keep HashMap to have ArrayList<String> to
+                     get over some weird stuff with generic types and ArrayLists
+                     Just convert to int when we actually need it instead */
+//                    ArrayList<Integer> coordinatesArray = new ArrayList<>();
+//                    for(String s: splitArray) {
+//                        coordinatesArray.add(Integer.parseInt(s));
+//                    }
+                    fileContents.put(city, splitArray);
+                }
+            } catch(FileNotFoundException exception) {
+                System.out.println("File not found. Exiting");
+                System.exit(1);
+            } catch(IOException exception) {
+                System.out.println("IO Exception. Exiting");
+                System.exit(1);
+            }
+        } else if(fileType.equals("connections")) {
+            try {
+                FileReader fileReader = new FileReader(filePath);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                while(!(line = bufferedReader.readLine()).equals("END")) {
+                    ArrayList<String> splitArray = new ArrayList<>(Arrays.asList(line.split("\\s+")));
+                    String city= splitArray.get(0);
+                    splitArray.remove(0);
+                    splitArray.remove(0);
+                    // debugging
+//                    System.out.println(splitArray);
+                    fileContents.put(city, splitArray);
+                }
+            } catch(FileNotFoundException exception) {
+                System.out.println("File not found. Exiting");
+                System.exit(1);
+            } catch(IOException exception) {
+                System.out.println("IO Exception. Exiting");
+                System.exit(1);
+            }
+        }
+        return fileContents;
+    }
+
+    private static DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> buildGraph(HashMap<String, ArrayList<String>> fileContents) {
+        DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);
+        //debugging
+        int edgeWeight = 0;
+        for(HashMap.Entry<String, ArrayList<String>> entry : fileContents.entrySet()) {
+            String startingVertex = entry.getKey();
+            if(!(graph.containsVertex(startingVertex))) {
+                graph.addVertex(startingVertex);
+            }
+            ArrayList<String> values = entry.getValue();
+            for(String value : values) {
+                if(!(graph.containsVertex(value))) {
+                    graph.addVertex(value);
+                }
+                if(!(graph.containsEdge(startingVertex, value))) {
+                    DefaultWeightedEdge edge = graph.addEdge(startingVertex, value);
+                    // SET EDGE WEIGHT HERE, edgeWeight for debugging
+                    graph.setEdgeWeight(edge, edgeWeight);
+                    // debugging
+                    edgeWeight++;
+                } else {
+                    System.out.println("For some reason this graph already contains that edge? Line 144");
+                }
+            }
+        }
+        // debugging : printing newly created graph's edges
+        Set<DefaultWeightedEdge> edges = graph.edgeSet();
+
+        for (DefaultWeightedEdge e : edges) {
+            System.out.println((String.format("\"%s\" -> \"%s\"", graph.getEdgeSource(e), graph.getEdgeTarget(e))));
+        }
+
+
+        return graph;
+    }
 
 
 }
